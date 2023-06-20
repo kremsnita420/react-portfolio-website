08a39a16238058a0bf2bf6aa8d06e8e79aafa392
kremsnita <duranovic.safet@gmail.com>
2023-06-20 11:22:49 +0200

    First commit

---
+++ src/components/Layout/index.jsx
@@ -0,0 +1,14 @@
+import Sidebar from '../Sidebar'
+import './index.scss'
+
+import React from 'react'
+
+const Layout = () => {
+  return (
+    <div>
+      <Sidebar />
+    </div>
+  )
+}
+
+export default Layout
